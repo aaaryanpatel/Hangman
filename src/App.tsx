d32f37a93849ae54269bc785165d226d98eedd1b
aaaryanpatel <aryanpatel131129@gmail.com>
2026-02-17 18:45:30 -0600

    Logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,61 @@
-import { useState } from "react"
-import words from './wordList.json'
-import { HangmanDrawing } from "./components/HangmanDrawing"
-import { HangmanWord } from "./components/HangmanWord"
-import { Keyboard } from "./components/Keyboard"
+import { useEffect, useState } from "react";
+import words from "./wordList.json";
+import { HangmanDrawing } from "./components/HangmanDrawing";
+import { HangmanWord } from "./components/HangmanWord";
+import { Keyboard } from "./components/Keyboard";
 
 function App() {
   const [wordToGuess, setWordToGuess] = useState(() => {
-    return words[Math.floor(Math.random() * words.length)]
-  })
+    return words[Math.floor(Math.random() * words.length)];
+  });
 
-  const [gussedLetters, setGussedLetters] = useState<string[]>([])
+  const [guessedLetters, setGussedLetters] = useState<string[]>([]);
+
+  const inCorrectLetters = guessedLetters.filter(
+    (letter) => !wordToGuess.includes(letter),
+  );
+
+  function addGuessedLetter(letter: string) {
+    if (guessedLetters.includes(letter)) return;
+
+    setGussedLetters((currentLetters) => [...currentLetters, letter]);
+  }
+
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      const key = e.key;
+
+      if (!key.match(/^[a-z]$/)) return;
+
+      e.preventDefault();
+      addGuessedLetter(key);
+    };
+    document.addEventListener("keypress", handler);
+    return () => {
+      document.removeEventListener("keypress", handler);
+    };
+  }, []);
 
   console.log(wordToGuess);
-  return <div 
-    style={{
-      maxWidth: '800px',
-      display: 'flex',
-      flexDirection: 'column',
-      gap: '2rem',
-      margin: '0 auto',
-      alignItems: 'center',
-
-
-    }}
-  
+  return (
+    <div
+      style={{
+        maxWidth: "800px",
+        display: "flex",
+        flexDirection: "column",
+        gap: "2rem",
+        margin: "0 auto",
+        alignItems: "center",
+      }}
     >
-      <div style={{ fontSize: '2rem', textAlign: 'center'}}>Lose win</div>
-      <HangmanDrawing />
-      <HangmanWord />
-      <Keyboard />
-
-  </div>
+      <div style={{ fontSize: "2rem", textAlign: "center" }}>Lose win</div>
+      <HangmanDrawing numberOfGuesses={inCorrectLetters.length} />
+      <HangmanWord guessedLetters={guessedLetters} wordToGuess={wordToGuess} />
+      <div style={{ alignSelf: "stretch" }}>
+        <Keyboard />
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -1,5 +1,50 @@
+import styles from "../Keyboard.module.css";
+
+const KEYS = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+
 export function Keyboard() {
-    return <div>
-        
+  return (
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: "repeat(auto-fit, minmax(75px, 1fr))",
+        gap: ".5rem",
+      }}
+    >
+      {KEYS.map((key) => {
+        return (
+          <button className={`${styles.btn}`} key={key}>
+            {key}
+          </button>
+        );
+      })}
     </div>
+  );
 }
