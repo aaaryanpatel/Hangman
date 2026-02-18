50fb00b23217c1231494298e75c8254a62a6a327
aaaryanpatel <aryanpatel131129@gmail.com>
2026-02-17 18:02:10 -0600

    Hangman word

--- src/components/HangmanWord.tsx
+++ src/components/HangmanWord.tsx
@@ -1,5 +1,28 @@
 export function HangmanWord() {
-    return <div>
-        
+  const word = "test";
+  const gussedLetters = ["t", "e", "g"];
+  return (
+    <div
+      style={{
+        display: "flex",
+        gap: ".25rem",
+        fontSize: "6rem",
+        fontWeight: "bold",
+        textTransform: "uppercase",
+        fontFamily: "monospace",
+      }}
+    >
+      {word.split("").map((letter, index) => (
+        <span style={{ borderBottom: ".1em solid black" }} key={index}>
+          <span
+            style={{
+              visibility: gussedLetters.includes(letter) ? "visible" : "hidden",
+            }}
+          >
+            {letter}
+          </span>
+        </span>
+      ))}
     </div>
+  );
 }
